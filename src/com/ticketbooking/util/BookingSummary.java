cae51c565456199c2e0bcf5ef0c0476f138da13c
chethana-s-bxa <chethana.s@bounteous.com>
2026-03-10 15:34:52 +0530

    Implement BookingSummary to display final ticket booking report

--- src/com/ticketbooking/util/BookingSummary.java
+++ src/com/ticketbooking/util/BookingSummary.java
@@ -1,4 +1,23 @@
 package com.ticketbooking.util;
 
+import com.ticketbooking.model.TicketInventory;
+
 public class BookingSummary {
+    private final TicketInventory inventory;
+    private final long executionTime;
+
+    public BookingSummary(TicketInventory inventory, long executionTime) {
+        this.inventory = inventory;
+        this.executionTime = executionTime;
+    }
+
+    public void printSummary(){
+        System.out.println("-------------------------------------------------");
+        System.out.println("SUMMARY");
+        System.out.println("-------------------------------------------------");
+        System.out.println("Initial tickets: "+inventory.getInitialTickets());
+        System.out.println("Total tickets sold: "+inventory.getTotalTicketsSold());
+        System.out.println("Remaining tickets: "+inventory.getAvailableTickets());
+        System.out.println("Execution time: "+executionTime+"ms");
+    }
 }
